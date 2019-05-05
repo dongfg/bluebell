75cbcd2dab903de9d32ffaf8fae53c35c30b9338
dongfg <mail@dongfg.com>
2019-05-05 17:20:37 +0800

    add common response method

--- controller/controller.go
+++ controller/controller.go
@@ -1,9 +1,34 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"time"
+)
 
 // Register all controller
 func Register(r *gin.Engine) {
 	newHealthController(r.Group("/health"))
 	newSeriesController(r.Group("/series"))
+}
+
+func success() gin.H {
+	return gin.H{
+		"msg":       "success",
+		"timestamp": time.Now().Unix(),
+	}
+}
+
+func failed(msg string) gin.H {
+	return gin.H{
+		"msg":       msg,
+		"timestamp": time.Now().Unix(),
+	}
+}
+
+func data(data interface{}) gin.H {
+	return gin.H{
+		"msg":       "success",
+		"data":      data,
+		"timestamp": time.Now().Unix(),
+	}
 }
